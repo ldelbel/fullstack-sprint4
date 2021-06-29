bc933b76bacd2e0d9a724ac895aad3a40a078169
ldelbel <lucdelbel@gmail.com>
2021-06-29 10:49:36 -0300

    Refactor products

--- src/components/Main/Products/index.jsx
+++ src/components/Main/Products/index.jsx
@@ -3,47 +3,31 @@ import { useMessages } from "../../../contexts/MessagesContext";
 import { getProducts } from "../../../services/productsService";
 import { Product } from "./Product";
 
-
 export function Products() {
   const [data, setData] = useState();
-  const [chunks, setChunks] = useState();
   const { addMessage } = useMessages();
 
   async function fetchData() {
     const req = await getProducts();
-    if(req.error) {
+    if (req.error) {
       addMessage(req.error);
       return;
     }
     setData(req);
   }
-  
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    if (data) {
-      const chunks = Array.from({ length: data.length / 4 }, () =>
-        data.splice(0, 4)
-      );
-
-      setChunks(chunks);
-    }
-  }, [data]);
-
   return (
-    <section className="main__products products">
-      {chunks &&
-        chunks.map((row, idx) => (
-          <div className="products__row" key={`key${idx}`}>
-            <ol className="products__list">
-              {row.map((product) => (
-                <Product key={product.sku} product={product} />
-              ))}
-            </ol>
-          </div>
-        ))}
+    <section>
+      <ul className="main__products">
+        {data &&
+          data.map((product) => (
+            <Product key={product.sku} product={product} />
+          ))}
+      </ul>
     </section>
   );
 }
